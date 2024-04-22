9009126928b02f1a3b535cadb1e8821cf145a4f2
chernovsushki <alexey.chernov@neptunessushi.com>
2024-04-22 19:24:53 +0300

    2204 task about incr decr delete in selected pruducts 220424 1924

--- src/modules/Basket/ProductsList/ProductsList.tsx
+++ src/modules/Basket/ProductsList/ProductsList.tsx
@@ -4,11 +4,10 @@ import { useSelector } from 'react-redux'
 import { selectBasketProducts } from 'redux/products/selectors'
 
 const ProductsList = () => {
-  const products = useSelector(selectBasketProducts)
-
+  const selectedProducts = useSelector(selectBasketProducts)
   return (
     <Flex flexDir="column" gap={4}>
-      {products.map((item) => (
+      {selectedProducts.map((item) => (
         <ProductListItem key={item.product.id} item={item} />
       ))}
     </Flex>
