83b4f00e03b62def4c97f9e62866259edb275582
Ura427 <urakost427@gmail.com>
2024-04-23 16:56:54 +0300

    Made header design for mobile layout

--- src/App/AppHeader/AppHeaderSm.tsx
+++ src/App/AppHeader/AppHeaderSm.tsx
@@ -1,23 +1,67 @@
-import { Box, Flex} from '@chakra-ui/react'
+import { chakra, Box, Flex, useMediaQuery } from '@chakra-ui/react'
 import Logo from 'components/Logo'
 import LanguageSelect from './LanguageSelect'
 import Place from './Place'
+import Burger from './Burger'
+
+import { NavLink } from 'react-router-dom'
+import { NAV_LINKS } from '../../constants/index'
+import 'i18n/config'
+import { useTranslation } from 'react-i18next'
+import { useState } from 'react'
+
+const MainNavLink = chakra(NavLink, {
+  baseStyle: {
+    fontSize: 16,
+    transition: 'all 0.2s',
+    fontWeight: 400,
+    letterSpacing: '.35px',
+    color: '#343330',
+    fontFamily: 'Rubik',
+    p: '6px',
+    alignSelf: 'end',
+
+    _hover: {
+      color: 'gray.300',
+    },
+    _activeLink: {
+      color: 'gray.600',
+    },
+  },
+})
+
 
 const AppHeaderSm = () => {
+  const { t } = useTranslation()
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <Flex align="center" py={6} px={2.5} justify="space-between" w="100%">
-      <Box w="33.3%">
-        <Logo />
-      </Box>
+    <>
+      <Flex align="center" py={6} px={2.5} w="100%">
+        <Box w="33.3%">
+          <Logo />
+        </Box>
 
-      <Flex align="center" justify="center" gap={3} w="33.3%">
-        <LanguageSelect />
-        <Place />
+        <Flex align="center" justify="center" gap={3} w="33.3%" grow={1}>
+          <LanguageSelect />
+          <Place />
+        </Flex>
+
+        <Flex w="33.3%" justify="end">
+          <Burger isOpen={isOpen} setIsOpen={setIsOpen}/>
+        </Flex>
       </Flex>
 
-      <Flex w="33.3%" justify="end">
-      </Flex>
-    </Flex>
+    {isOpen && (
+       <Flex flexDir="column" alignSelf="end">
+       {NAV_LINKS.map((route: string, idx: number) => (
+         <MainNavLink to={route} key={`navbar.${idx}`}>
+           {t(`navbar.${idx}`)}
+         </MainNavLink>
+       ))}
+     </Flex>
+    )}
+     
+    </>
   )
 }
 
---
+++ src/App/AppHeader/Burger.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import burger from '../../assets/icons/burger.svg'
+import close from '../../assets/icons/close.svg'
+import { Box, Image } from '@chakra-ui/react'
+
+interface BurgerProps {
+  isOpen: boolean
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const Burger = ({ isOpen, setIsOpen }: BurgerProps) => {
+  const handleChange = () => {
+    setIsOpen((prev) => !prev)
+  }
+
+  return (
+    <>
+      {isOpen ? (
+        <>
+          <Box onClick={handleChange}>
+            <Image src={close} h="8px" w="8px" />
+          </Box>
+        </>
+      ) : (
+        <Box onClick={handleChange}>
+          <Image src={burger} alt="Burger Icon" />
+        </Box>
+      )}
+    </>
+  )
+}
+
+export default Burger
